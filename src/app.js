5d53792aea894e0f82bf737e843d8153ac9dcfa7
Thang Tran <trandinhthangdev@gmail.com>
2024-01-09 17:57:50 +0700

    init project

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const compression = require("compression");
+const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+const app = express();
+
+// init middleware
+app.use(morgan("dev"));
+// app.use(morgan("combined")); // production
+// app.use(morgan("common"));
+// app.use(morgan("short"));
+// app.use(morgan("tiny"));
+
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init routes
+app.get("/", (req, res, next) => {
+    const strCompress = "Hello Thang Tran";
+    return res.status(200).json({
+        message: "Welcome !",
+        metadata: strCompress.repeat(10000),
+    });
+});
+
+// handling error
+
+module.exports = app;
